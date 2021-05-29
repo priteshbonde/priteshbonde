326e84f8d138fd18ff44510b7717c7a9a7153de3
Pritesh Bonde <pbonde@mimeo.com>
2021-05-29 21:14:28 +0530

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100:'#E2E2D5',
+          200:'#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
